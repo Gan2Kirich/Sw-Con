534294de808a7421b8db144d7a1344e227373435
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-09-30 18:20:24 +0300

    Add files via upload

---
+++ CW--13/CW--13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите чмсло: ");
+int n;
+
+ if (int.TryParse(Console.ReadLine(), out n))
+{
+    int nex = n + 1;
+    int pox = n - 1;
+
+    Console.WriteLine("Следующее за  числом " + n + " число - " + nex);
+    Console.WriteLine("Для числа " + n + " предыдущее число - " + pox);
+}
+ else
+{
+    Console.WriteLine("Ошибка: Введенное значение не является целым числом.");
+}

d7cba20956dbb023972c472d138c6054a0cfde74
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-01 19:06:44 +0300

    Add files via upload

---
+++ Cw-16/Cw-16/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число t ");
+int t = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число v ");
+int v = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число x1 ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число x2 ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число y ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("а) 5 10\n" + "   7 см\n");
+
+Console.WriteLine($"б) 100 {t}\n" + $"   1949 {v}\n");
+
+Console.WriteLine($"в) {x} 25\n" + $"   {X} {y}\n");

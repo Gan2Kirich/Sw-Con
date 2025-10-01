bd7ffdfa68fb61c3e8e92e0d04e1d779d4ef5879
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-01 17:42:35 +0300

    Add files via upload

---
+++ CW-15/CW-15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите 1 число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2 число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 3 число: ");
+int v = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 4 число: ");
+int t = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Вы ввели следующие числа: " +  n + " " + c + " "  + v + " " + t);

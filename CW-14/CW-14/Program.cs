75ce26d14aa04e2246708dbad20725408f112604
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-01 16:53:13 +0300

    Add files via upload

---
+++ CW-14/CW-14/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число 1: ");
+int a =Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(a + "  " + b + "  " + c);
+

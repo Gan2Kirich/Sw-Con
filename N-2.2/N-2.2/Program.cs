a5e953971834956ad94280772a2458f2e7b096e7
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-06 18:19:27 +0300

    Add files via upload

---
+++ N-2.2/N-2.2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+double b = Math.Sqrt(a * a + 1);
+
+double y = ((a * a) + 10)/b;
+
+Console.WriteLine("Результат функции: " + y);

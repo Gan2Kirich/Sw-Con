209d52765723e5a8f10d329a0df35e23f0a57f12
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-06 18:02:29 +0300

    Add files via upload

---
+++ N-2.1/N-2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// раздел a
+Console.Write("Введите число x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int y = 17 * (x * x) - 6 * x + 13;
+
+Console.WriteLine("Результат y: " + y);
+
+// раздел б 
+
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int c = 3 * (a*a) + 5*a - 21;
+
+Console.WriteLine("Результат c: " + c);
+

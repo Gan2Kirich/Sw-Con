582956b6b2d86c64efe104ffb957eaae123f1bf8
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-07 18:36:10 +0300

    Add files via upload

---
+++ N-2.3/N-2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿// раздел a
+Console.Write("Введите число a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+double y = 2 * a + Math.Sin(Math.Abs(3*a));
+double z = y / 3.56;
+double c = Math.Sqrt(z);
+
+Console.WriteLine("Результат функции: " + c);
+
+
+// раздел б
+
+Console.Write("Введите число x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+double num = 3.2 + Math.Sqrt(1 + x);
+double fd = num / Math.Abs(5*x);
+double fin = Math.Sin(fd);
+Console.WriteLine("Результат функции: " + fin);

c340812a09085b5d480336c193735eb3e93a245e
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-08 22:01:53 +0300

    Add files via upload

---
+++ N-2.6/N-2.6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите высоту: ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+double R = 6350;
+
+double d = Math.Sqrt(2*R*h+(h*h));
+
+Console.WriteLine("Растояние до линии горизонта " + d);

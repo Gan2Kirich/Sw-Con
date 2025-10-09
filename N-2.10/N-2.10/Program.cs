6315081b946e8d3eb52f74946d69bd6f4a23470a
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-09 21:10:34 +0300

    Add files via upload

---
+++ N-2.10/N-2.10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double c = (a + b) / 2;
+
+double d = Math.Sqrt(a * b);
+
+Console.Write("Среднее арифмитеческое " + c);
+Console.Write("Среднее геометрическое " + d);

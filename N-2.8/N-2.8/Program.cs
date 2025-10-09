d63486a31fcd1496bbb2ce9fc7867b670e3a5fb1
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-09 07:59:10 +0300

    Add files via upload

---
+++ N-2.8/N-2.8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите радиус окружности ");
+double R = Convert.ToDouble(Console.ReadLine());
+
+double L =2 * Math.PI * R;
+
+double S = Math.PI * (R * R);
+Console.WriteLine("Длина окружности " + L + "\n");
+Console.WriteLine("Площадь круга " + S);

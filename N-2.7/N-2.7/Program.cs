74827f6ff1f76af8b805435311fd5f87a2f821b9
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-09 07:38:40 +0300

    Add files via upload

---
+++ N-2.7/N-2.7/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите длину ребра куба ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int v = a * a * a;
+
+int sb = 4 * a * a;
+
+Console.WriteLine("Обьем куба " + v + "\n");
+Console.WriteLine("Площадь боковой поверхности " + sb );

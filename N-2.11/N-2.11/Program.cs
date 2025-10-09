bc43371da8834609bcbad6b4d53551b4d149d816
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-09 21:25:41 +0300

    Add files via upload

---
+++ N-2.11/N-2.11/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите массу тела ");
+double m = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите обьем тела ");
+double v = Convert.ToDouble(Console.ReadLine());
+
+double p = m / v;
+
+Console.WriteLine("Плотность материала этого тела " + p);

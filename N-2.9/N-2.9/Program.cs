39f0099681577ced530c1393706354fb5de058f1
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-09 20:53:45 +0300

    Add files via upload

---
+++ N-2.9/N-2.9/Program.cs
@@ -0,0 +1,23 @@
+﻿// раздел a 
+Console.Write("Введите число x ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите число y ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+double z = (2 * Math.Pow(x, 3)) - (3.44 * x * y) + (2.3 *Math.Pow(x,2))- (7.1 * y) + 2;
+
+Console.WriteLine("Рузультат вычислений " + z);
+
+// рахдел б
+Console.Write("Введите число a ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите число b ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double n = a + b;
+
+double X = (3.14*Math.Pow(n,3)) + (2.75*Math.Pow(b,2)) - (12.7*a) - 4.1;
+
+Console.WriteLine("Рузультат вычислений " + X);

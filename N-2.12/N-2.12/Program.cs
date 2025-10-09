82beda900e2ff358e165a42061cff39638e9b7f3
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-09 21:33:18 +0300

    Add files via upload

---
+++ N-2.12/N-2.12/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите численость населения ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите площадь территории ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double c = a / b;
+
+Console.WriteLine("Плотность населения " + c);

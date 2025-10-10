f1fb630f9f16c70b796ab2080ae965fd4b696fb6
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-10 08:03:46 +0300

    Add files via upload

---
+++ N-2.13/N-2.13/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число а(не может быть рано 0) ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите число b ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (a == 0)
+{
+    if (b == 0)
+    {
+        Console.WriteLine("Множество решений");
+
+    }
+    else
+    {
+        Console.WriteLine("Нет решений");
+    }
+}
+else
+{
+    double c = -b / a;
+    Console.WriteLine("Ответ " + c);
+
+}

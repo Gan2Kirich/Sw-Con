3a029f572edd25be65811aba2afe40cbf24d664c
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-10 22:26:40 +0300

    Add files via upload

---
+++ N-2.14/N-2.14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_2._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число а ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число b ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double d = Math.Pow(a, 2);
+            double n = Math.Pow(b, 2);
+
+            double c = d + n;
+
+            double fin = Math.Sqrt(c);
+            Console.WriteLine("Гипотенуза равна " + fin);
+        }
+    }
+}

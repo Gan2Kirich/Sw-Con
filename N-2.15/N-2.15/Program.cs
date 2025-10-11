5cecaac41950ddc31b7788f3f4aa90aeb99118ef
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-11 12:11:39 +0300

    Add files via upload

---
+++ N-2.15/N-2.15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_2._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите внешний радиус ");
+            double R = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите внутриний радиус ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            if (R <= r)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            else
+            {
+                double S = Math.PI * (Math.Pow(R, 2) - Math.Pow(r, 2));
+
+                Console.WriteLine("Площадь кольца " + S);
+            }
+        }
+    }
+}

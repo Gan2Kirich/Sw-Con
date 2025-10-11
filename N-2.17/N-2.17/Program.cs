55b6c861b10b42aec9dcb897805083db0e74cbe1
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-11 13:05:39 +0300

    Add files via upload

---
+++ N-2.17/N-2.17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_2._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите основание а ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите основание b ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите высоту h ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            if (a <= b)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            else
+            {
+                double x = (a - b) / 2;
+
+                double c = Math.Sqrt(Math.Pow(h, 2) + Math.Pow(x, 2));
+
+                double P = a + b + 2 * c;
+                Console.WriteLine("Периметр " + P);
+            }
+        }
+    }
+}

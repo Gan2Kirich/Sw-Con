901b20ad5d9dbe85a5deee937b69f215d59bd034
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-11 13:48:01 +0300

    Add files via upload

---
+++ N-2.18/N-2.18/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_2._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // пример z
+            Console.Write("Введите число x ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число y ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            if (x == 0)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            else
+            {
+                double c = (2 + y) / Math.Pow(x, 2);
+                double a = x + c;
+
+                double d = 1 / (Math.Sqrt(Math.Pow(x, 2) + 10));
+                double b = y + d;
+
+                double z = a / b;
+                Console.WriteLine("Результат " + z);
+            }
+            // пример  q
+            Console.Write("Введите число x ");
+            double X = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число y ");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            double q = 7.25 * Math.Sin(X) - Math.Abs(Y);
+            Console.WriteLine("Результат" + q);
+        }
+    }
+}

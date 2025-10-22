1475d9fbc06fdb7a12da1c0659aa960cc7fd4972
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-22 23:00:57 +0300

    Add files via upload

---
+++ N-2.22/N-2.22/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите 1-е число ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите 2-е число ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double arm = (a + b)/2;
+double geom = Math.Sqrt(Math.Abs(a)*Math.Abs(b));
+Console.WriteLine($"Арифмитическое {arm}");
+Console.WriteLine($"Геометричекое {geom}" );

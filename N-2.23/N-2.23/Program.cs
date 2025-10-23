5f8b65a479a4213d5048ab386401e7a6bb8a9295
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-23 17:15:34 +0300

    Add files via upload

---
+++ N-2.23/N-2.23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите длину ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите ширину ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double p = 2*(a + b);
+double d = Math.Sqrt(Math.Pow(a,2)+ Math.Pow(b, 2));
+Console.WriteLine($"Длина {p}");
+Console.WriteLine($"Длина {d}");

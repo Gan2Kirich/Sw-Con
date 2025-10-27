7985b5c37e3e64cb2a8a214927e17d103d39c147
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-27 21:10:25 +0300

    Add files via upload

---
+++ N-2.27/N-2.27/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите 1-e основание ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Введите 2-e основание ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите высоту ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+if (b > a)
+{
+    double t = a;
+    a = b;
+    b = t;
+}
+    double x = (a - b) / 2;
+    double c = Math.Sqrt(Math.Pow(h,2)+Math.Pow(x,2));
+    double P = a + b + 2 * c;
+    Console.WriteLine($"Периметр равен {P}");
+

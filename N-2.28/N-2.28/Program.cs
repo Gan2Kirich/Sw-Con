313ff3099082572f734f22b1f90a01ecdb912ec0
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-10-28 17:22:46 +0300

    Add files via upload

---
+++ N-2.28/N-2.28/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Ведите большее основание: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Ведите меньшее основание: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ведите угол: ");
+double alfa = Convert.ToDouble(Console.ReadLine());
+
+if (a <= 0 || b <= 0)
+{
+    Console.WriteLine("Ошибка");
+}
+else if (alfa <= 0 || alfa >= 180)
+{
+    Console.WriteLine("Ошибка");
+}
+else
+{
+    if (b > a)
+    {
+        double t = a;
+        a = b;
+        b = t;
+    }
+    double alfag = alfa * (Math.PI / 180.0);
+    double x = (a - b) / 2;
+    double h = x * Math.Tan(alfag);
+    double H = Math.Abs(h);
+    double S = ((a + b) / 2) * H;
+    Console.WriteLine($"Площадь: {S}");
+}
+

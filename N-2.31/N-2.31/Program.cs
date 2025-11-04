69727fff9af8cff28d16385a144422238c9e3ca7
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-11-04 11:07:37 +0300

    Add files via upload

---
+++ N-2.31/N-2.31/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Стоимость яблок, печенья, конфет за 1кг - 100 рублей.");
+double price = 100;
+
+Console.Write("Введите количество кг яблок: ");
+double apple = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите количество кг печенья: ");
+double cookie = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите количество кг конфет: ");
+double candies = Convert.ToDouble(Console.ReadLine());
+
+if (apple < 0 || cookie < 0 || candies < 0)
+{
+    Console.WriteLine("Ошибка");
+}
+else if (apple == 0 && cookie == 0 && candies == 0)
+{
+    Console.WriteLine("Вы ничего не взяли");
+}
+else
+{
+    double finp1 = price * apple;
+    double finp2 = price * cookie;
+    double finp3 = price * candies;
+    double fin = finp1 + finp2 + finp3;
+    Console.WriteLine($"Стоимость {fin}");
+}

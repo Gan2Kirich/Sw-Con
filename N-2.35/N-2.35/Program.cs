13c2c746f40f11aaf240ff3579cb0e31358d21e0
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-11-06 19:29:00 +0300

    Add files via upload

---
+++ N-2.35/N-2.35/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Ввдетие скорость 1 автомобиля(Больше чем 2): ");
+double V1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ввдетие скорость  автомобиля: ");
+double V2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ввдетие на сколько км 1 автомобиль опередил 2: ");
+double S = Convert.ToDouble(Console.ReadLine());
+
+if (V1 <= 0 || V2 <= 0 || S <= 0)
+{
+    Console.WriteLine("Ошибка");
+}
+else if (V1 <= V2)
+{
+    Console.WriteLine("Ошибка");
+}
+else
+{
+    double V = V1 - V2;
+    double S1 = 0.5 * V;
+    double fin = S + S1;
+    Console.WriteLine($"Расстояние через 30 мин: {fin}");
+}

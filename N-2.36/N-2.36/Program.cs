3a76308067ccf70634979778c9e0bfd7b6254811
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-11-07 19:53:52 +0300

    Add files via upload

---
+++ N-2.36/N-2.36/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите температуру(по цельсию) ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double tf = c * 1.8 + 32;
+double tk = c + 273.15;
+Console.WriteLine($"Температура по фаренгету {tf}");
+Console.WriteLine($"Температура в кельвинах {tk}");

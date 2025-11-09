5ab8ff78cf5611736f8c04cbcd231d5a75556dee
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-11-09 18:49:02 +0300

    Add files via upload

---
+++ Section 3/N-3.1/N-3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите растояние в сантиметрах: ");
+double s = Convert.ToDouble(Console.ReadLine());
+
+if (s <= 0)
+{
+    Console.WriteLine("Ошибка");
+}
+else
+{
+    int l = (int)s / 100;
+    Console.WriteLine($"Расстояние в метрах: {l}");
+}

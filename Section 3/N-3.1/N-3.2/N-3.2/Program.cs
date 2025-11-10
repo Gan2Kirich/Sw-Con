8979ac508f1e13133d7fc38fc91c30ecf49b1dd8
Gan2Kirich <kiriltchekulae@yandex.ru>
2025-11-10 17:59:40 +0300

    Add files via upload

---
+++ Section 3/N-3.1/N-3.2/N-3.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ведите массу в кг: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a <= 0)
+{
+    Console.WriteLine("Ошибка");
+}
+else
+{
+    int fin = a / 100;
+    Console.WriteLine($"Целое число в центнерах: {fin}");
+}
